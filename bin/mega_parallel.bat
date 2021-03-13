d6863cb74bee0a3eda9c08109804881667d17c46
Andrew Feldman <andrew.feldman@microsoft.com>
2021-03-13 09:31:04 +0000

    update

--- bin/mega_parallel.bat
+++ bin/mega_parallel.bat
@@ -1,5 +1,6 @@
+set /A T=100000000
 set /A N=20
-set /A S=50000000/%N%
+set /A S=%T%/%N%
 
 echo %N%
 echo %S%
@@ -8,6 +9,6 @@ setlocal enableDelayedExpansion
 FOR /l %%A IN (1,1,%N%) DO (
 	set /A M=S*%%A-S
 	echo !M!
-	start ycsb load mongodb -s -P "C:\Users\anfeldma\codeHome\YCSB\workloads\workloadr" -p insertstart=!M! -p insertcount=%S% -p mongodb.url="mongodb+srv://anfeldma:O%%21curmt0@atlas44noshard1oregonus.fr0to.mongodb.net/ycsb?authSource^^=admin^^&retryWrites^^=true^^&w^^=1" -p operationcount=100000000 -p recordcount=100000000 -threads 100
+	start ycsb load mongodb -s -P "C:\Users\anfeldma\codeHome\YCSB\workloads\workloadr" -p insertstart=!M! -p insertcount=%S% -p mongodb.url="mongodb+srv://anfeldma:O%%21curmt0@azure44noshard1oregonus.fr0to.mongodb.net/ycsb?authSource^^=admin^^&retryWrites^^=true^^&w^^=1" -p operationcount=%T% -p recordcount=%T% -threads 100
 	)
 	
